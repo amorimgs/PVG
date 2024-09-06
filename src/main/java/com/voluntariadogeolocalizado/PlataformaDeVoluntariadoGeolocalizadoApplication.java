eec0e6f9443de1507d6a34db3c157422982266c4
Gabriel Amorim <ricksamann@gmail.com>
2024-09-06 19:33:02 -0300

    feat: frist commit

---
+++ src/main/java/com/voluntariadogeolocalizado/PlataformaDeVoluntariadoGeolocalizadoApplication.java
@@ -0,0 +1,13 @@
+package com.voluntariadogeolocalizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeVoluntariadoGeolocalizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeVoluntariadoGeolocalizadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voluntariadogeolocalizado/PlataformaDeVoluntariadoGeolocalizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voluntariadogeolocalizado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeVoluntariadoGeolocalizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
